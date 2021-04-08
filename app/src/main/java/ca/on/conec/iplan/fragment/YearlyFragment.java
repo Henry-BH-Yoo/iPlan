e19c5c20be320fe78cc5a746a035dabebe5a1f53
Sean Yang <hewas6@gmail.com>
2021-04-08 11:48:53 -0400

    feat: add temp layout, floating button, icons

--- app/src/main/java/ca/on/conec/iplan/fragment/YearlyFragment.java
+++ app/src/main/java/ca/on/conec/iplan/fragment/YearlyFragment.java
@@ -2,65 +2,58 @@ package ca.on.conec.iplan.fragment;
 
 import android.os.Bundle;
 
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
 import ca.on.conec.iplan.R;
+import ca.on.conec.iplan.database.AppDatabase;
+import ca.on.conec.iplan.entity.User;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link YearlyFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class YearlyFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public YearlyFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment YearlyFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static YearlyFragment newInstance(String param1, String param2) {
-        YearlyFragment fragment = new YearlyFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private FloatingActionButton fab;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_yearly, container, false);
+        View v = inflater.inflate(R.layout.fragment_yearly, container, false);
+
+        // FloatingActionButton
+        fab = (FloatingActionButton) v.findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Add action will be created", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        // Create db
+        AppDatabase db = AppDatabase.getAppDatabase(getActivity().getApplicationContext());
+
+        User user = new User();
+
+        user.setFirstName("Henry");
+        user.setLastName("Yoo");
+
+        new Thread(() -> {
+            Log.i("INFO", "INSERT Obj");
+            db.userDao().insertAll(user);
+        }).start();
+
+        return v;
     }
 }
